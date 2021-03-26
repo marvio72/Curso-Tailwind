e814ab954b377d0faa6e9440faefdfd63a0fd3a4
Marco Ruvalcaba <desarrollo@oversistemas.com>
2021-03-26 00:51:07 -0600

    Dándole forma al logo

--- web-tailwind/tailwind.config.js
+++ web-tailwind/tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
       backgroundImage: (theme) => ({
         'body-pattern': "url('../img/pattern.png')",
       }),
+      colors: {
+        'azul-claro': '#37bcf9',
+        'azul-obscuro': '#0370b9',
+      },
     },
   },
 };
