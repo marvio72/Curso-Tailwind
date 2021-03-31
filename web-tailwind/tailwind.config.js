18716629907221bf70f5a5d8a9f69f46ea99e7f9
marvio72 <mruvalcaba@oversistemas.com>
2021-03-31 14:13:42 -0600

    Animando el logo - Parte1

--- web-tailwind/tailwind.config.js
+++ web-tailwind/tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
         'azul-claro': '#37bcf9',
         'azul-obscuro': '#0370b9',
       },
+      animation: {
+        'spin-low': 'spin 2s linear infinite',
+      },
     },
+  },
+  variants: {
+    animation: ['responsive', 'hover', 'group-hover'],
   },
 };
