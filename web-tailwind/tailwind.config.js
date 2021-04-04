bcb45eaaff4ba6a59d771dcb88aa97dbed304857
marvio72 <mruvalcaba@oversistemas.com>
2021-04-04 11:50:07 -0500

    Introducción al responsive de Tailwind

--- web-tailwind/tailwind.config.js
+++ web-tailwind/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   theme: {
     extend: {
+      screens: {
+        xs: { max: '768px' },
+      },
       backgroundImage: (theme) => ({
         'body-pattern': "url('../img/pattern.png')",
       }),
@@ -11,7 +14,7 @@ module.exports = {
       animation: {
         'spin-low': 'spin 2s linear infinite',
         'from-bellow': 'fromBellow 500ms linear',
-        'from-right': 'fromRight 300ms linear'
+        'from-right': 'fromRight 300ms linear',
       },
       keyframes: {
         fromBellow: {
@@ -20,9 +23,9 @@ module.exports = {
           '100%': { transform: 'translateY(0%)' },
         },
         fromRight: {
-          '0%': { transform: 'translateX(200%)'},
-          '100%': { transform: 'translateX(0%)'},
-        }
+          '0%': { transform: 'translateX(200%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       },
     },
   },
