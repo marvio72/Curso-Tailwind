48fb5e822d1c8038a110ab9ba5910b41030ae619
marvio72 <mruvalcaba@oversistemas.com>
2021-04-04 13:13:16 -0500

    Estilos con Tailwind al banner animado

--- web-tailwind/tailwind.config.js
+++ web-tailwind/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       },
       backgroundImage: (theme) => ({
         'body-pattern': "url('../img/pattern.png')",
+        banner: "url('../img/bakbaner.png')",
       }),
+      backgroundPosition: {
+        'banner-position': '-200px -200px',
+      },
       colors: {
         'azul-claro': '#37bcf9',
         'azul-obscuro': '#0370b9',
